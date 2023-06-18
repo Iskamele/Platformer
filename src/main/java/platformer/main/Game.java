f890eec9a63dec1217d3ac656595d17a5ba576a3
iskamele <iskamele@gmail.com>
2023-06-18 12:25:24 +0300

    Implemented abstract Entity class. Implemented Player class. Implemented moving with booleans. Added attack animation.

--- src/main/java/platformer/main/Game.java
+++ src/main/java/platformer/main/Game.java
@@ -1,17 +1,26 @@
 package platformer.main;
 
+import java.awt.Graphics;
+import platformer.entities.Player;
+
 public class Game implements Runnable {
     private GameWindow gameWindow;
     private GamePanel gamePanel;
     private Thread gameLoopThread;
     private final int FPS_SET = 120;
     private final int UPS_SET = 200;
+    private Player player;
 
     public Game() {
-        gamePanel = new GamePanel();
+        initClasses();
+        gamePanel = new GamePanel(this);
         gameWindow = new GameWindow(gamePanel);
         gamePanel.requestFocus();
         starGameLoop();
+    }
+
+    private void initClasses() {
+        player = new Player(200, 200);
     }
 
     private void starGameLoop() {
@@ -53,7 +62,19 @@ public class Game implements Runnable {
         }
     }
 
+    public Player getPlayer(){
+        return player;
+    }
+
     private void update() {
-        gamePanel.updateGame();
+        player.update();
+    }
+
+    public void render(Graphics g) {
+        player.render(g);
+    }
+
+    public void windowFocusLost() {
+        player.resetDirectionBooleans();
     }
 }
--- src/main/java/platformer/main/GameWindow.java
+++ src/main/java/platformer/main/GameWindow.java
@@ -1,5 +1,7 @@
 package platformer.main;
 
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowFocusListener;
 import javax.swing.JFrame;
 
 public class GameWindow {
@@ -14,5 +16,16 @@ public class GameWindow {
         jFrame.setResizable(false);
         jFrame.pack();
         jFrame.setVisible(true);
+        jFrame.addWindowFocusListener(new WindowFocusListener() {
+            @Override
+            public void windowGainedFocus(WindowEvent e) {
+                gamePanel.getGame().windowFocusLost();
+            }
+
+            @Override
+            public void windowLostFocus(WindowEvent e) {
+                System.out.println("BYE");
+            }
+        });
     }
 }
